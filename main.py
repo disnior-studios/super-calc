ba4734380c1e52b57080f9e8b0b01feb04c70520
pkieke10 <98700039+pkieke10@users.noreply.github.com>
2022-11-07 17:34:52 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import math
+from tkinter import Y
+
+infuction = input("What do you want to do?")
+x = 
+y = 
+
+
+
+if infuction == '/':
+    fishedval = x / y
+    print(fishedval)
+   
+elif infuction == '*':
+    fishedval = x * y
+    print(fishedval)
+elif infuction == '-':
+    fishedval = x - y
+    print(fishedval)
+elif infuction == '+':
+    fishedval = x + y
+    print(fishedval)
